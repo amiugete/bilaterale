28b22afe5551780baf91d6eacb50fef5c26142cc
rmarzocchi84 <roberto.marzocchi@amiu.genova.it>
2023-01-27 10:31:22 +0100

    ricarica pagina

--- eliminazione.php
+++ eliminazione.php
@@ -3,7 +3,7 @@ session_set_cookie_params($lifetime);
 session_start();
 
 require_once('./funzioni_jwt.php');
-echo $_SESSION['test']."<br>";
+//echo $_SESSION['test']."<br>";
 //exit;
 
 if ($_SESSION['test']==1) {
@@ -83,13 +83,55 @@ $json = '';
 curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $resp = curl_exec($curl);
-echo $resp;
+//echo $resp;
 $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-echo "<br>". $httpCode;
+//echo "<br>". $httpCode;
 curl_close($curl);
 //exit;
 
 
+//header("location:javascript://history.go(-1)");
+//header("location:javascript://history.back(); Location.reload ()");
 
+require_once('./req.php');
 
+require_once('./navbar_up.php');
+
+if ($httpCode==200){
+    ?>
+    <div class="container">
+    <div class="row">
+<div id="ok">
+  <h3> Piazzola eliminata</h3>
+  <form autocomplete="off" id="prospects_form3" action="piazzola.php" method="post">
+  <input type="hidden" id="piazzola" name="piazzola" value="<?php echo $id?>">
+  <button class="btn btn-success"> <i class="fa-solid fa-arrow-rotate-right"></i> Ricarica piazzola</button>
+  </form>
+</div>
+<?php
+} else {
 ?>
+<div id="problema">
+<h3> Problema eliminazione piazzola</h3>
+<form autocomplete="off" id="prospects_form3" action="piazzola.php" method="post">
+<input type="hidden" id="piazzola" name="piazzola" value="<?php echo $id?>">
+<button class="btn btn-danger"> <i class="fa-solid fa-arrow-rotate-right"></i> Ricarica piazzola</button>
+</form>
+</div>
+<?php
+}
+?>
+</div></div>
+
+
+<?php
+require_once('req_bottom.php');
+require('./footer.php');
+?>
+
+
+
+
+</body>
+
+</html>
